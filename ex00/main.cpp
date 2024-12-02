31dead7beb713f240b0a37084db89022c400c10c
jcummins <jcummins42prague@gmail.com>
2024-12-02 11:36:31 +0100

    Started and finished ex00 templates

---
+++ ex00/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcummins <jcummins@student.42prague.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 17:43:14 by jcummins          #+#    #+#             */
+/*   Updated: 2024/12/02 11:17:33 by jcummins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "templates.hpp"
+#include <iostream>
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return (0);
+}
---
+++ ex00/templates.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   templates.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcummins <jcummins@student.42prague.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 17:44:19 by jcummins          #+#    #+#             */
+/*   Updated: 2024/12/02 11:34:50 by jcummins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEMPLATES_H
+# define TEMPLATES_H
+
+template <typename T>
+void swap(T& x, T& y)
+{
+	T temp = x;
+	x = y;
+	y = temp;
+}
+
+template <typename T>
+const T& min(const T& x, const T& y)
+{
+	return (x < y ? x : y);
+}
+
+template <typename T>
+const T& max(const T& x, const T& y)
+{
+	return (x > y ? x : y);
+}
+
+#endif
+
